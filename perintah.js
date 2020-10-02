10b3255f07380408913789eece11b61598279bd7
Geoveza <geoveza21@gmail.com>
2020-10-02 11:11:44 +0700

    Create perintah.js

---
+++ perintah.js
@@ -0,0 +1,8 @@
+import tele.js
+//masukkan pesan perintah dan balasan
+bot.hears('PESAN PERINTAH', ctx =>{
+	OpenFile = 'ALAMAT FILE, Contoh C:/User/blabla/bla.exe';
+	Buka_File()
+	
+	return ctx.reply('PESAN BALASAN BOT');
+});
