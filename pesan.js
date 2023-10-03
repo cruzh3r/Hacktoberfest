e920f6f74b44526ce52ed387639f8eca19450413
Geoveza <geoveza21@gmail.com>
2023-10-03 22:01:57 +0700

    Update pesan.js
    1

--- pesan.js
+++ pesan.js
@@ -1,3 +1,1 @@
-//masukkan pesan
-blalaaalala
-test
+the name of the project, we appreciate this project to be a useful project for everyone and we hope that in the future the project will bring a very big change for the community.
